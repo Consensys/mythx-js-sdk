27de5c27d8f557c935261fba6354ed36988aa1b5
Daniyar Chambylov <dan.chambylov@gmail.com>
2019-02-22 14:38:46 +0700

    Update trufstuf module
    * Update getJsonFilesToAnalyze
    * Add parseBuildJson
    * Add isStaleFile
    * Add filterStaleJsonFiles

--- src/analyze/trufstuf.ts
+++ src/analyze/trufstuf.ts
@@ -4,6 +4,8 @@ import util from 'util';
 
 
 const readdir: Function = util.promisify(fs.readdir);
+const readFile: Function = util.promisify(fs.readFile);
+const stat: Function = util.promisify(fs.stat);
 
 
 /**
@@ -13,9 +15,61 @@ const readdir: Function = util.promisify(fs.readdir);
  * @param {string} directory - path to truffle smart contracts build directory. {
  * @resolves {Promise<string[]>} - resolves list of JSON files.
  */
-export const getJsonFilesToAnalyze = async function(directory: string): Promise<string[]> {
+export const getJsonFilesToAnalyze = async (directory: string): Promise<string[]> => {
     const files: string[] = await readdir(directory);
     const filteredFiles: string[] = files.filter(f => f !== 'Migrations.json');
     const filePaths: string[] = filteredFiles.map((f: string) => path.join(directory, f));
     return filePaths;
+};
+
+
+export const parseBuildJson = async (file: string): Promise<object> => {
+    const buildJson: string = await readFile(file, 'utf8');
+    const buildObj: any = JSON.parse(buildJson);
+
+    // Recent versions of truffle seem to add __ to the end of the bytecode
+    for (const field of ['bytecode', 'deployedBytecode']) {
+        if (buildObj[field]) {
+            buildObj[field] = buildObj[field].replace(/_.+$/, '');
+        }
+    }
+    return buildObj;
+};
+
+/**
+ * Detects whether file is too old and needs to drop out
+ * @param {string} filePath - file path
+ */
+export const isStaleFile = async (filePath: string): Promise<boolean> => {
+    const buildObj: any = await parseBuildJson(filePath);
+    const fullPathStat = await stat(filePath);
+    const buildMtime = fullPathStat.mtime;
+    const sourcePath = buildObj.sourcePath;
+    let sourcePathStat;
+
+    try {
+        sourcePathStat = await stat(sourcePath);
+    } catch (err) {
+        return true;
+    }
+
+    const sourceMtime = sourcePathStat.mtime;
+    return sourceMtime > buildMtime;
+};
+
+/**
+ * Filters old json files from input array
+ * @param files - array of json files
+ */
+export const filterStaleJsonFiles = async (files: string[]): Promise<Array<string>> => {
+    const result: Array<string> = [];
+    await Promise.all(files.map(async f => {
+        const isStale = await isStaleFile(f);
+        if (isStale) {
+            return ;
+        }
+        result.push(f);
+        return f;
+    }));
+    return result;
 };
--- test/analyze/trufstuf.test.ts
+++ test/analyze/trufstuf.test.ts
@@ -1,11 +1,15 @@
 import assert from 'assert';
+import sinon, { SinonStub } from 'sinon';
 import proxyquire from 'proxyquire';
+import { start } from 'repl';
 
-
+/*
 describe('trufstuf', function() {
   let trufstuf: any;
+  let readdir: Function;
 
   beforeEach(function () {
+    readdir = sinon.stub();
     trufstuf = proxyquire('../../src/analyze/trufstuf', {
       fs: {
         readdir: (directory: string, cb: Function) => cb(undefined, [
@@ -23,5 +27,130 @@ describe('trufstuf', function() {
       '/test/build/contracts/Contract.json',
       '/test/build/contracts/OtherContract.json',
     ]);
+  });
+});
+*/
+
+describe('trufstuf', function() {
+  let trufstuf: any;
+  let readdir: SinonStub;
+  let readFile: SinonStub;
+  let stat: SinonStub;
+
+  beforeEach(function () {
+    readdir = sinon.stub();
+    readFile = sinon.stub();
+    stat = sinon.stub();
+    trufstuf = proxyquire('../../src/analyze/trufstuf', {
+      fs: {
+        readdir,
+        readFile,
+        stat,
+      },
+    });
+  });
+
+  afterEach(() => {
+    readdir.reset();
+    readFile.reset();
+    stat.reset();
+  });
+
+  describe('getJsonFilesToAnalyze', () => {
+    it('should remove Migrations.json ad return full paths', async () => {
+      readdir.yields(undefined, [ 'Contract.json', 'Migrations.json', 'OtherContract.json' ]);
+      const files: string[] = await trufstuf.getJsonFilesToAnalyze('/test/build/contracts');
+
+      assert.deepEqual(files, [
+        '/test/build/contracts/Contract.json',
+        '/test/build/contracts/OtherContract.json',
+      ]);
+    });
+  });
+
+  describe('parseBuildJson', () => {
+    it('should parse json file to object', async () => {
+      readFile.yields(undefined, '{"contractName": "Test", "bytecode": "0x", "deployedBytecode": "0x"}');
+      const expected = {
+        contractName: 'Test',
+        bytecode: '0x',
+        deployedBytecode: '0x',
+      };
+
+      const result = await trufstuf.parseBuildJson('contract.json');
+      assert.deepEqual(result, expected);
+    });
+
+    it('should drop __from bytecode and deployedBytecode', async () => {
+      readFile.yields(undefined, '{"contractName": "Test", "bytecode": "0x__", "deployedBytecode": "0x__"}');
+      const expected = {
+        contractName: 'Test',
+        bytecode: '0x',
+        deployedBytecode: '0x',
+      };
+
+      const result = await trufstuf.parseBuildJson('contract.json');
+      assert.deepEqual(result, expected);
+    });
+  });
+
+  describe('isStaleFile', () => {
+    let parseBuildJson: SinonStub;
+
+    beforeEach(() => {
+      parseBuildJson = sinon.stub(trufstuf, 'parseBuildJson');
+    });
+
+    afterEach(() => {
+      parseBuildJson.restore();
+    });
+
+    it('should identify json as non-stale when mtime of json and source file are the same', async () => {
+      stat.yields(undefined, { mtime: 1000000 });
+      parseBuildJson.resolves({ sourcePath: 'test/contract.sol' });
+      const result = await trufstuf.isStaleFile('test/build/contracts', 'Contract.json');
+      assert.deepEqual(result, false);
+    });
+
+    it('should identify json as stale when source file does not exist', async () => {
+      stat
+        .onFirstCall().yields(undefined, { mtime: 1000000 });
+      stat
+        .onSecondCall().yields('error');
+      parseBuildJson.resolves({ sourcePath: 'test/contract.sol' });
+
+      const result = await trufstuf.isStaleFile('test/build/contracts', 'Contract.json');
+      assert.ok(result);
+    });
+
+    it('should identify json as stale when json file is older than source', async () => {
+      stat
+        .onFirstCall().yields(undefined, { mtime: 1000000 });
+      stat
+        .onSecondCall().yields(undefined, { mtime: 1000001 });
+      parseBuildJson.resolves({ sourcePath: 'test/contract.sol' });
+
+      const result = await trufstuf.isStaleFile('test/build/contracts', 'Contract.json');
+      assert.ok(result);
+    });
+  });
+
+  describe('filterStaleJsonFiles', () => {
+    let isStaleFile: SinonStub;
+    beforeEach(() => {
+      isStaleFile = sinon.stub(trufstuf, 'isStaleFile');
+    });
+
+    afterEach(() => {
+      isStaleFile.restore();
+    });
+
+    it('should remove old contract from result', async () => {
+      const inputFiles = ['OldContract.json', 'NewContract.json'];
+      isStaleFile.onFirstCall().resolves(true);
+
+      const result = await trufstuf.filterStaleJsonFiles(inputFiles);
+      assert.deepEqual(result, ['NewContract.json']);
+    });
   });
 });
